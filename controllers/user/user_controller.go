f0086818fe15bdd488877434b0ca8aed374a968d
xiao9878 <55609365@qq.com>
2020-08-09 17:30:47 +0800

    紧急提交

--- controllers/user/user_controller.go
+++ controllers/user/user_controller.go
@@ -4,6 +4,8 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 	"manager/models/user"
+	"manager/utils"
+	"math"
 )
 
 type UserController struct {
@@ -11,11 +13,71 @@ type UserController struct {
 }
 
 func (this *UserController) Get() {
+	/*
+		o := orm.NewOrm()
+
+		limitNum := 10
+
+		currentPage,err := this.GetInt("page")
+		if err != nil {
+			currentPage = 1
+		}
+
+		users := []user.User{}
+
+		count, _ := o.QueryTable(new(user.User)).Filter("is_delete", 0).Count()
+		o.QueryTable(new(user.User)).Filter("is_delete", 0).Limit(limitNum).Offset((currentPage-1)*limitNum).All(&users)
+
+		countPage := int(math.Ceil(float64(count)/float64(limitNum)))
+
+		prePage := 1
+		if currentPage == 1 {
+			prePage = currentPage
+		} else if currentPage > 1 {
+			prePage = currentPage -1
+		}
+		nextPage := 1
+		if currentPage < countPage {
+			nextPage = currentPage + 1
+		} else {
+			nextPage = currentPage
+		}*/
 	o := orm.NewOrm()
 
+	limitNum := 10
+
+	currentPage, err := this.GetInt("page")
+	if err != nil {
+		currentPage = 1
+	}
+
 	users := []user.User{}
-	o.QueryTable(new(user.User)).Filter("is_delete", 0).All(&users)
+
+	count, _ := o.QueryTable(new(user.User)).Filter("is_delete", 0).Count()
+	o.QueryTable(new(user.User)).Filter("is_delete", 0).Limit(limitNum).Offset((currentPage - 1) * limitNum).All(&users)
+
+	countPage := int(math.Ceil(float64(count) / float64(limitNum)))
+
+	prePage := 1
+	if currentPage == 1 {
+		prePage = currentPage
+	} else if currentPage > 1 {
+		prePage = currentPage - 1
+	}
+	nextPage := 1
+	if currentPage < countPage {
+		nextPage = currentPage + 1
+	} else {
+		nextPage = currentPage
+	}
+
+	utils.Paginator(currentPage, prePage, count)
 
 	this.Data["users"] = users
+	this.Data["prePage"] = prePage
+	this.Data["nextPage"] = nextPage
+	this.Data["currentPage"] = nextPage
+	this.Data["countPage"] = countPage
+	this.Data["count"] = count
 	this.TplName = "user/user.html"
 }
---
+++ utils/page.go
@@ -0,0 +1,5 @@
+package utils
+
+func Paginator(page, pageNum int, nums int64) map[string]interface{} {
+	return nil
+}
