9a27a6fb725f9ade498af39ea1d1e221ed6113bf
xiao9878 <55609365@qq.com>
2020-09-08 10:48:04 +0800

    硬盘监控完善

--- controllers/serve_info.go
+++ controllers/serve_info.go
@@ -18,14 +18,14 @@ type SysInfo struct {
 }
 
 type Info struct {
-	Name    string                 `json:"host"`
-	Os      string                 `json:"os"`
-	Adl     string                 `json:"adl"`
-	CoreNum int                    `json:"coreNum"`
-	Version string                 `json:"version"`
-	Cpu     map[string]interface{} `json:"cpu"`
-	Memory  map[string]string      `json:"memory"`
-	Disk    map[string]string      `json:"disk"`
+	Name    string                  `json:"host"`
+	Os      string                  `json:"os"`
+	Adl     string                  `json:"adl"`
+	CoreNum int                     `json:"coreNum"`
+	Version string                  `json:"version"`
+	Cpu     *map[string]interface{} `json:"cpu"`
+	Memory  *map[string]string      `json:"memory"`
+	Disk    *[]map[string]string    `json:"disk"`
 }
 
 // @router /sys/info [*]
@@ -33,7 +33,20 @@ func (this *SysInfo) GetInfo() {
 	s := time.Now().Nanosecond()
 	cpu, _ := cpu.Info()
 	memory, _ := mem.VirtualMemory()
-	disk, _ := disk.Usage("D:")
+	disks, _ := disk.Partitions(true)
+	fmt.Println(disks)
+	//disk, _ := disk.Usage("D:")
+	var diskList []map[string]string
+	for _, p := range disks {
+		usage, _ := disk.Usage(p.Mountpoint)
+		dinfo := map[string]string{
+			"name":    p.Mountpoint,
+			"total":   fmt.Sprintf("%.2f", float64(usage.Total)/1024/1024/1024) + "G",
+			"used":    fmt.Sprintf("%.2f", float64(usage.Used)/1024/1024/1024) + "G",
+			"percent": strconv.Itoa(int(usage.UsedPercent)) + "%",
+		}
+		diskList = append(diskList, dinfo)
+	}
 	cpuInfo := map[string]interface{}{
 		"name":  cpu[0].ModelName,
 		"mhz":   cpu[0].Mhz,
@@ -44,12 +57,6 @@ func (this *SysInfo) GetInfo() {
 		"available": strconv.FormatInt(int64(memory.Available/(1024*1024)), 10) + "M",
 		"used":      strconv.FormatInt(int64(memory.Used/(1024*1024)), 10) + "M",
 		"percent":   strconv.FormatInt(int64(memory.UsedPercent), 10) + "%",
-	}
-	diskInfo := map[string]string{
-		"total":     strconv.FormatInt(int64(disk.Total/(1024*1024)), 10) + "M",
-		"available": strconv.FormatInt(int64(disk.Free/(1024*1024)), 10) + "M",
-		"used":      strconv.FormatInt(int64(disk.Used/(1024*1024)), 10) + "M",
-		"percent":   strconv.FormatInt(int64(disk.UsedPercent), 10) + "%",
 	}
 	//主机名
 	hostName, _ := os.Hostname()
@@ -67,9 +74,9 @@ func (this *SysInfo) GetInfo() {
 		Adl:     adl,
 		CoreNum: core,
 		Version: version,
-		Cpu:     cpuInfo,
-		Memory:  memoryInfo,
-		Disk:    diskInfo,
+		Cpu:     &cpuInfo,
+		Memory:  &memoryInfo,
+		Disk:    &diskList,
 	}
 	e := time.Now().Nanosecond()
 	fmt.Println("--------------", e-s, "--------------")
